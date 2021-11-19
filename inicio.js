9e834af71c66db0b4a1230dee468fbbc49b0f01e
Alehff <84363595+Alehff@users.noreply.github.com>
2021-11-19 10:23:49 -0300

    Update inicio.js

--- inicio.js
+++ inicio.js
@@ -1,9 +1,9 @@
 document.getElementById ("texto1").innerHTML="Nome: Alehff"
 document.getElementById ("texto2").innerHTML="Idade: 26"
 document.getElementById ("texto3").innerHTML="Estado: Ceará"
-document.getElementById ("imagem").src="../public/perfil3.png"
+document.getElementById ("imagem").src="public/perfil3.png"
 document.getElementById ("texto4").innerHTML="Hobbie: games"
 document.getElementById ("texto5").innerHTML="Descrição: Curto jogar videogame no playstation"
 document.getElementById ("texto6").innerHTML="Links: Não tem"
-document.getElementById ("imagem2").src="../public/R.jfif"
+document.getElementById ("imagem2").src="public/R.jfif"
       
